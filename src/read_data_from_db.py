33660d27cb69eeaabb2c1e69d96ef8a0c050bcca
KarpenkoDenis <Karpenko.Denis75@gmail.com>
2017-12-27 01:48:11 +0300

    fix db reader

--- src/read_data_from_db.py
+++ src/read_data_from_db.py
@@ -8,6 +8,45 @@ from os import listdir
 word_count - если меньше 0, то все слова
 иначе то кол-во, которое указано
 '''
+
+
+def add_ends_to_word(word, ends):
+    fin_words = []
+    # print("words_ending", words_ending)
+    ends = ends_parser(ends)
+    for end in ends:
+        fin_words.append(word + (end if end != '#' else ""))
+    return fin_words
+
+def ends_parser(words_ending):
+    start = 0
+    end =0
+    save_ending = []
+    curr_end = ""
+    inline = False
+    for i in range(0, len(words_ending)):
+        if inline:
+            if words_ending[i] == ',' and words_ending[i-1] == '}':
+                inline = False
+            continue
+        elif words_ending[i] ==',':
+            save_ending = save_ending + [curr_end]
+            curr_end = ""
+        elif words_ending[i] == '{':
+            if start !=0:
+                # ends = ends_parser(words_ending[start:])
+                save_ending = save_ending + add_ends_to_word(curr_end, words_ending[i:])
+                inline = True
+                curr_end = ""
+            start = i + 1
+        elif words_ending[i] == '}':
+            save_ending = save_ending + [curr_end]
+            end = i
+            break
+        else:
+            curr_end = curr_end + words_ending[i]
+    return save_ending
+
 def read_from(path, word_count):
 
     words = open(path)
@@ -21,39 +60,53 @@ def read_from(path, word_count):
         strings.append(line)
     if word_count < 0 or word_count > len(strings) -1:
         word_count = len(strings) -1
-    # print(strings)
     words_ending = strings[0]
-    # print(words_ending)
-    start =0
-    end =0
 
-    for i in range(0, len(words_ending)):
-        if words_ending[i] == '{':
-            start = i+1
-        if words_ending[i] == '}':
-            end = i
-            break
-    words_ending = words_ending[start:end]
-    words_ending = words_ending.split(",")
+    ends_mass= ends_parser(words_ending)
+
     fin_words = []
-    # print("words_ending", words_ending)
+    # print("words_ending", ends_mass)
     for word in strings[1:word_count+1]:
-         for end in words_ending:
+         for end in ends_mass:
              fin_words.append(word[:-1] + (end if end != '#' else ""))
-    #print("Final words:", fin_words)
+    print("Final words:", fin_words)
     return fin_words
 
 
 def read_files_from(path, word_count):
     all_words = []
     # print(listdir("./resource/ЖЕН/"))
+    i=0
     for file in listdir(path):
+        print(i)
         all_words = all_words + read_from(path + file, word_count)
+        i+=1
 
     # print("all_words:", all_words)
     return all_words
 
+def read_files_from_name(path, name):
+    all_words =  read_from(path + name, -1)
 
-read_files_from("./resource/ЖЕН/", -1)
-# read_files_from('./resource/МУЖ/')
+    # print("all_words:", all_words)
+    return all_words
 
+'''
+
+# пример
+
+ends_parser("{ел,л{а,у},в{щл{1,2,3},jiodf,ji,о,ш},шо,оа}")
+
+print("-------------")
+
+ends_parser("{ел,л{а,у},в{щл,jiodf,ji,о,ш},шо,оа}")
+add_ends_to_word("123", "{ел,л{а,у},в{щл,jiodf,ji,о,ш},шо,оа}")
+
+
+'''
+
+'''
+read_files_from_name("./resource/ЖЕН/", "lexgroup.031")
+read_files_from('./resource/МУЖ/')
+
+'''
